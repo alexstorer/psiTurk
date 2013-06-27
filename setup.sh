e2f84638ba93dcafbd381c4108138a764cda43db
Alex Storer <astorer@iq.harvard.edu>
2013-06-27 18:27:39 +0000

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo pip install Flask-SQLAlchemy
+sudo pip install boto
+sudo apt-get install mysql-server
+sudo apt-get install build-essential python-dev libmysqlclient-dev
+sudo pip install mysql-python
+sudo pip install mysql_connector_python
+mysql -u root -p -e "show databases;"
